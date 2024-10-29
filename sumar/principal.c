4eac58ae00884be23114c9b44854e8a46172e674
aaleex18 <a.martinezmartin@edu.uah.es>
2024-10-29 09:40:19 +0100

    Mensaje de primer commit

---
+++ sumar/principal.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "suma.h"
+int main() {
+	int a=5, b=10, c;
+	c = sumar(a,b);
+	printf("El resultado de la suma es: %d\n", c);
+	return 0;
+}
+
---
+++ sumar/sumar.c
@@ -0,0 +1,6 @@
+#include "suma.h"
+int sumar(int sumando1, int sumando2){
+	return sumando1 + sumando2;
+}
+
+
